457db62cb3b2b69cc3feda580535e616f7406d4f
gabriel tavares <gabriel29t@gmail.com>
2022-02-21 12:52:33 -0300

    Chalenge solved and stylized

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    if params[:question] == 'I am going to work'
+      @question = 'Great!'
+    elsif params[:question].include? '?'
+      @question = 'Silly question, get dressed and go to work!'
+    else
+      @question = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
